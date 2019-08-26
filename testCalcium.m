a9d393591d7a6d7cd024870c7a412b1f7c6cb2b0
wqlevi <37841818+wqlevi@users.noreply.github.com>
2019-08-26 21:18:48 +0200

    Add files via upload

--- testCalcium.m
+++ testCalcium.m
@@ -1,4 +1,4 @@
-load('C:\Users\wangqi\Documents\Lab\Data\ca_pupil_08212018\rsfmri_16.mat')
+load('C:\Users\wangqi\Documents\Lab\Data\ca_pupil_08212018\rsfmri_13.mat')
 addpath("C:\Users\wangqi\Downloads\Matlab_scripts\");
 Fs = 5000;
 TR = 1/Fs;
@@ -13,6 +13,26 @@ b = [1/4 1/4 1/4 1/4];
 y1 = filter(b,a,x1);%moving average filter
 y2 = filter(b,a,x2);
 t = 1:length(x1);
+
+t_1 = 0:1/Fs:(length(x1)-1)/Fs;
+plot(t_1,x1);
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Raw data timecourse');
+%% plot power 2 freq.
+m_1 = length(x1);       % original sample length
+n_p1 = pow2(nextpow2(m_1));  % transform length, exponential 2 is to facilitate calculation
+fft_x1_p = fft(x1,n_p1);
+f_p1 = (0:n_p1-1)*(Fs/n_p1)/10;% DFT of signal
+power = abs(fft_x1_p).^2/n_p1;   
+pwr2db = 10*log10(power);
+figure,
+plot(f_p1(1:floor(n_p1/2)),power(1:floor(n_p1/2)));% how to adjust intensity range of power for display??
+xlim([0 5]);
+ylim([0 5]);
+xlabel('Frequency');
+ylabel('Power');
+
 subplot(2,1,1);
 plot(t,x1,'--',t,y1,'-');
 xlim([0 length(x1)*TR]);
@@ -49,12 +69,25 @@ x = x1;
 h= figure();
 % plot(f,10*log10(pxx),'LineWidth',plot_linewidth);
 plot(f,pow2db(pxx))
+%% Straight forward way to freq. domain
+n_x1 = length(x1_ave);% number of samples
+fft_x1 = fft(x1_ave);
+f = (0:n_x1-1)*(Fs/n_x1);     % frequency range
+power = (abs(fft_x1).^2)/n_x1;    % power of the DFT
+
+plot(f,power)
+xlabel('Frequency')
+ylabel('Power')
+xlim([0 5]);
+ylim([-10 10]);
 %% PSD 
 window = hann(2^13);
 ovrl = 1/2*length(window);
 nfft = 2^18;
-[s,f,t] = spectrogram(bpf_x1,window,ovrl,nfft,Fs);
+[s,f,t] = spectrogram(x1_ave,window,ovrl,nfft,Fs);
 
-imagesc(t,f(f<10),abs(s(f<10)));
+psd_x1 = imagesc(t,f(f<10),abs(s(f<10)));
 colormap(jet);
+arr_psd_x1 = imread(psd_x1);
+Col_psd_x1(:,1) = mean(arr_psd_x1(:,1));
 
